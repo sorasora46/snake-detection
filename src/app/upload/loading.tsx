0a96934c308ccf2ed8bf4aae406e74326b61784f
sorasora46 <sorrawit02546@gmail.com>
2023-11-30 02:11:20 +0700

    remove extra lottie

--- src/app/upload/loading.tsx
+++ src/app/upload/loading.tsx
@@ -7,7 +7,6 @@ export default function Loading() {
   return (
     <main>
       <Lottie animationData={loadingAnimation} loop />
-      <Lottie animationData={loadingAnimation} loop />
       <p>We are predicting</p>
       <p>Please wait</p>
     </main>
